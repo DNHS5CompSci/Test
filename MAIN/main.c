43101e35ecd70a96b0b0dceaa68784fd85a12072
Nakul Nandhakumar <nakul@nandhakumar.net>
2019-09-10 14:51:54 -0700

    Initial Commit

---
+++ MAIN/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MAIN
+//
+//  Created by Nandhakumar, Nakul on 9/10/19.
+//  Copyright © 2019 Nandhakumar, Nakul. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
